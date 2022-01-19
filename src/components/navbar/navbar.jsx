e7425ec27514c1fcc67a69e06bb147194c810372
Vinícius Soares <vinisoares15@gmail.com>
2022-01-19 18:06:31 +0000

    Começando a trabalhar com a navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+
+//BEM -> Block Element Modifier
 
 const navbar = () => {
     return (
-        <div>
-                <h1>navbar</h1>
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+
+                </div>
+            </div>
         </div>
     )
 }
