410b76a6d8b62532d3eb4f4b90170dd263656a17
Vinícius Soares <vinisoares15@gmail.com>
2022-01-29 17:30:14 +0000

    adição da pasta de assets com as imagens utilizadas no projeto

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 //BEM -> Block Element Modifier
 
@@ -8,8 +9,8 @@ const navbar = () => {
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
-                <div className="gpt3__navbar-links_logo">
-                    <img src={logo} alt="logo" />
+                <div className="gpt3__navbar-links_ logo">
+                    <img src={logo} alt="logo" />   
 
                 </div>
             </div>
--- src/containers/index.js
+++ src/containers/index.js
@@ -8,4 +8,3 @@ export { default as WhatGPT3 } from './WhatGPT3/WhatGPT3';
 //se tiver algum erro nesse export, é pq os nomes das pastas foram escritos
 //diferentes de como estão nos ficheiros. 
 
-//Fazendo coisas diferentes de novo
