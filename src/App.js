111aeecfd4ff9c42399e3e9a58f9fd1e6701a7cd
Vinícius Soares <vinisoares15@gmail.com>
2022-02-15 15:11:11 +0000

    teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import {Footer, Blog, Features, Possibility, WhatGPT3, Header } from './containers';
-import {Cta, Brand, Navbar} from './components';
-import './App.css';
+import {Cta, Brand, Navbar} from './components';  //nos imports, todos os parâmetros devem ser importados com letra maúscula no começo
+import './App.css';                               //ex: "C"ta, "B"rand, "N"avbar
 
 
 const App = () => {
