77cf341bb0ac3a7dd3891390ebba5148fb9255aa
Vinícius Soares <vinisoares15@gmail.com>
2022-02-24 09:27:59 -0300

    construção do menu da Navbar junto com o Style no css e construção dos botões de Sing Up e Sing in

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,8 +9,23 @@ const navbar = () => {
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
-                <div className="gpt3__navbar-links_ logo">
+                <div className="gpt3__navbar-links_logo">
                     <img src={logo} alt="logo" />   
+
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p>
+                        <a href="#home">Home</a>
+                        <a href="#wgpt3">What is GPT3</a>
+                        <a href="#possibility">Open IA</a>
+                        <a href="#features">Case Studies</a>
+                        <a href="#blog">Library</a>
+                    </p>
+
+                </div>
+                <div className="gpt3__navbar-sign">
+                    <p>Sign in</p>
+                    <button type="button">Sign up</button>
 
                 </div>
             </div>
